85d64680002ad96044d9a29917a33c9cdd1008ba
Anirudh Pottammal <anipotts@anirudhs-mbp.lan>
2024-03-19 02:32:25 -0400

    added mobile hero margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
       <GradientBackground />
       <div className="z-10 flex flex-col justify-center w-3/4 h-[80vh] mx-auto hero-content">
         <div className="m-0 mb-4 text-3xl text-right md:text-center dongle">
-          <p className="my-2 thin">Hi, my name is </p>
+          <p className="py-1 thin">Hi, my name is </p>
           <span className="font-bold text-transparent gradient-text animate-gradient whitespace-nowrap">
             Anirudh Pottammal<span className="text-white">.</span>
           </span>
